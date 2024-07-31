a7d0686355c8e460cc4d50e2ea5a61d371379567
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-07-31 13:48:42 -0300

    feat: create quantity-input

---
+++ src/components/quantity-input/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  padding: 0.5rem;
+  background-color: ${({ theme }) => theme.colors['base-button']};
+  border-radius: 6px;
+  display: flex;
+  gap: 0.25rem;
+
+  button {
+    background-color: transparent;
+    display: flex;
+    align-items: center;
+
+    svg {
+      color: ${({ theme }) => theme.colors.purple};
+      transition: all 0.2s;
+
+      &:hover {
+        color: ${({ theme }) => theme.colors['purple-dark']};
+      }
+    }
+  }
+
+  span {
+    padding-top: 2px;
+    color: ${({ theme }) => theme.colors['base-title']};
+  }
+`
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,21 @@
+import { QuantityInput } from '@/components/quantity-input'
+import { useState } from 'react'
+
 export function Home() {
-  return <h1>Home</h1>
+  const [quantity, setQuantity] = useState(0)
+
+  function incrementQuantity() {
+    setQuantity((state) => state + 1)
+  }
+
+  function decrementQuantity() {
+    setQuantity((state) => state - 1)
+  }
+  return (
+    <QuantityInput
+      quantity={quantity}
+      incrementQuantity={incrementQuantity}
+      decrementQuantity={decrementQuantity}
+    />
+  )
 }
